1240f29afdfbb32fd81b59e0fae9b946fc42c275
Luke Yeh <lukeforwardto@gmail.com>
2026-03-20 00:47:43 +0800

    fix(c20awsentrasso): switch RoleSessionName from userprincipalname to mail

--- corpSetup/c20awsentrasso/policy.tf
+++ corpSetup/c20awsentrasso/policy.tf
@@ -4,132 +4,131 @@ resource "msgraph_resource_action" "claims_policy" {
   resource_url = "servicePrincipals/${azuread_application_from_template.aws_sso_corp.service_principal_object_id}/claimsPolicy"
 
   body = {
-    includeBasicClaimSet          = true
-    includeApplicationIdInIssuer  = false
-    audienceOverride              = null
-    groupFilter                   = null
+    includeBasicClaimSet         = true
+    includeApplicationIdInIssuer = false
+    audienceOverride             = null
+    groupFilter                  = null
 
     claims = [
       {
-        "@odata.type"                 = "#microsoft.graph.samlNameIdClaim"
-        serviceProviderNameQualifier  = null
-        nameIdFormat                  = "emailAddress"
+        "@odata.type"                = "#microsoft.graph.samlNameIdClaim"
+        serviceProviderNameQualifier = null
+        nameIdFormat                 = "emailAddress"
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "userprincipalname"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "userprincipalname"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "givenname"
-        namespace                  = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "givenname"
+        namespace               = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "givenname"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "givenname"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "surname"
-        namespace                  = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "surname"
+        namespace               = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "surname"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "surname"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "emailaddress"
-        namespace                  = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "emailaddress"
+        namespace               = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "mail"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "mail"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "name"
-        namespace                  = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "name"
+        namespace               = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "userprincipalname"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "userprincipalname"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
- 
       # AWS attributes
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "RoleSessionName"
-        namespace                  = "https://aws.amazon.com/SAML/Attributes"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "RoleSessionName"
+        namespace               = "https://aws.amazon.com/SAML/Attributes"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
             attribute = {
-              "@odata.type"         = "#microsoft.graph.sourcedAttribute"
-              id                    = "userprincipalname"
-              source                = "user"
-              isExtensionAttribute  = false
+              "@odata.type"        = "#microsoft.graph.sourcedAttribute"
+              id                   = "mail"
+              source               = "user"
+              isExtensionAttribute = false
             }
             transformations = []
           }
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "SessionDuration"
-        namespace                  = "https://aws.amazon.com/SAML/Attributes"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "SessionDuration"
+        namespace               = "https://aws.amazon.com/SAML/Attributes"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
@@ -142,11 +141,11 @@ resource "msgraph_resource_action" "claims_policy" {
         ]
       },
       {
-        "@odata.type"              = "#microsoft.graph.customClaim"
-        name                       = "Role"
-        namespace                  = "https://aws.amazon.com/SAML/Attributes"
-        tokenFormat                = ["saml"]
-        samlAttributeNameFormat    = null
+        "@odata.type"           = "#microsoft.graph.customClaim"
+        name                    = "Role"
+        namespace               = "https://aws.amazon.com/SAML/Attributes"
+        tokenFormat             = ["saml"]
+        samlAttributeNameFormat = null
         configurations = [
           {
             condition = null
