3b7b871cdc15f0745783168ac513ab8f409493e6
Wakenarey <rjaykitkat@gmail.com>
2026-06-20 15:03:10 +1000

    fix(c07): comment out sensitive Azure AD application credentials in variables.tf

--- corpSetup/c07userAccounts/variables.tf
+++ corpSetup/c07userAccounts/variables.tf
@@ -7,21 +7,21 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "client_id" {
-  description = "The client ID of the Azure AD application used for authentication"
-  type        = string
-}
+# variable "client_id" {
+#   description = "The client ID of the Azure AD application used for authentication"
+#   type        = string
+# }
 
-variable "client_secret" {
-  description = "The client secret of the Azure AD application used for authentication"
-  type        = string
-  sensitive   = true
-}
+# variable "client_secret" {
+#   description = "The client secret of the Azure AD application used for authentication"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "tenant_id" {
-  description = "The tenant ID of the Azure AD application used for authentication"
-  type        = string
-}
+# variable "tenant_id" {
+#   description = "The tenant ID of the Azure AD application used for authentication"
+#   type        = string
+# }
 
 variable "dns_name" {
   description = "The DNS name for the environment"
