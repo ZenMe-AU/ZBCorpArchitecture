5219ec064cdf4fb4f354e6b6c8bb664573c076e2
Wakenarey <rjaykitkat@gmail.com>
2026-06-20 14:23:36 +1000

    fix(init): comment out sensitive Azure and MS Graph provider credentials. not needed for web app.

--- corpSetup/c07userAccounts/init.tf
+++ corpSetup/c07userAccounts/init.tf
@@ -27,13 +27,13 @@ terraform {
 provider "azurerm"  {
   features {}
   subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
 
 provider "msgraph" {
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  tenant_id     = var.tenant_id
+  # client_id     = var.client_id
+  # client_secret = var.client_secret
+  # tenant_id     = var.tenant_id
 }
