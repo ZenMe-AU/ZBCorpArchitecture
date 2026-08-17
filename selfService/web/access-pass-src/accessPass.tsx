c2b23a0c66b76fbcac029558dcb5cfdb0f4f9a51
Wakenarey <rjaykitkat@gmail.com>
2026-08-17 18:15:01 +1000

    refactor: update imports and add global styles

--- selfService/web/access-pass-src/accessPass.tsx
+++ selfService/web/access-pass-src/accessPass.tsx
@@ -1,11 +1,11 @@
 import "./monitor/bootstrapErrors"; // keep on first line to catch errors during bootstrap
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../corp-src/index.css'
-import AccessPassApp from './AccessPassApp.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+import AccessPassApp from "./AccessPassApp.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AccessPassApp />
-  </StrictMode>,
-)
+  </StrictMode>
+);
